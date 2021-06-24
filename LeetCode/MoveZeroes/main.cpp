8ec4e9644ee3c08d76d28852475fa1aee39d7625
Brawlence <42910934+Brawlence@users.noreply.github.com>
2021-06-24 11:16:14 +0300

    LEETCODE: MoveZeroes
    Finally, a working solution

--- LeetCode/MoveZeroes/main.cpp
+++ LeetCode/MoveZeroes/main.cpp
@@ -10,27 +10,6 @@
 
 using namespace std;
 
-void strToVector (string &input, vector<int> &bits) {
-	for (auto it = begin(input); it!=end(input); ++it) {
-		switch (*it) {
-			case '0':
-			case 'F':
-			case 'f':
-				bits.push_back(0);
-				break;
-			
-			case '1':
-			case 'T':
-			case 't':
-				bits.push_back(1);
-				break;
-
-			default:
-				break;
-		}
-	};
-};
-
 void printVector (vector<int> &bits) {
 	for (auto it = begin(bits); it!=end(bits); ++it) {
 		cout << *it << " ";
@@ -39,20 +18,15 @@ void printVector (vector<int> &bits) {
 }
 
 int main() {
-	string input = " ";
-	vector<int> nums;
+	vector<int> nums = {0,110,101,0,1,0,29,20,123,23,4,1,8,121,5,1,1,8,12,3,5,1,0,213,5,1,0};
 	
-	cout << "Enter the bit string to reinterpret.\nAllowed values: 1/0 T/F t/f:\n_";
-	cin >> noskipws >> input;
-
-	strToVector(input, nums);
-	
-	cout << "\nReinterpreted string as vector<int>:\n";
+	cout << "\nVector<int>:\n";
 	printVector(nums);
 
 	cin.get();
 
 	Solution moveZeroes;
+	moveZeroes.moveZeroes(nums);
 	printVector(nums);
 
 	cin.get();
--- LeetCode/MoveZeroes/solution.hpp
+++ LeetCode/MoveZeroes/solution.hpp
@@ -3,7 +3,7 @@
 
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
+    void moveZeroes(std::vector<int>& nums) {
         auto t = std::remove(nums.begin(), nums.end(), 0);
         std::fill(t, nums.end(), 0);
     }
