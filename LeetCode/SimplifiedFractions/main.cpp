99fc922e81b5d08d07e1b5b5c575e81bd7540d0b
Brawlence <42910943+Brawlence@users.noreply.github.com>
2021-08-13 08:14:41 +0300

    Leetcode: Simplified Fractions STUB

---
+++ LeetCode/SimplifiedFractions/main.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/simplified-fractions/
+// this is a complimentary support file which handles user input and output
+// for the solutions (defined in solution.hpp) to work
+
+#include <iostream>
+
+#include "solution.hpp"
+
+using namespace std;
+
+int main() {
+
+	Solution SimplifiedFractions;
+
+	cin.get();
+
+	return 0;
+}
---
+++ LeetCode/SimplifiedFractions/solution.hpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/simplified-fractions/
+
+#include <vector>
+#include <string>
+#include <numeric> //gcd 
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> simplifiedFractions(int denominator) {
+        vector<string> fractions = {};
+        vector<string> nums_as_string(denominator + 1);
+        
+        for (int i = 1; i <= denominator; i++) {
+            nums_as_string[i] = to_string(i);
+        };
+
+        while (denominator > 1) {
+            for (int nominator = denominator; nominator > 0; --nominator) {
+                if (gcd(nominator,denominator) == 1) {
+                    fractions.emplace_back(nums_as_string[nominator]+"/"+nums_as_string[denominator]);
+                };
+            };
+            --denominator;
+        };
+        
+        return fractions;
+    }
+};
