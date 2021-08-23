a113f4d4b53021ed9b125ed0bc182d20a8548630
Brawlence <42910943+Brawlence@users.noreply.github.com>
2021-08-23 19:05:33 +0300

    LeetCode: Defanging IP Adress

---
+++ LeetCode/DefangingIP.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/defanging-an-ip-address/
+
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string res = "",replace = "[.]";
+        for(int i = 0; i < address.size(); i++){
+            if(address[i] == '.')
+                res += replace;
+            else
+                res += address[i];      
+        }
+        return res; 
+    }
+};
