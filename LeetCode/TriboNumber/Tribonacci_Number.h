74659e257c8086fd36f0c1499427c1f13b9d03d5
Brawlence <42910943+Brawlence@users.noreply.github.com>
2021-09-16 18:39:59 +0300

    LEETCODE: Updated Tribonacci number
    Added the Memoization solution

--- LeetCode/TriboNumber/Tribonacci_Number.h
+++ LeetCode/TriboNumber/Tribonacci_Number.h
@@ -1,29 +1,28 @@
 // https://leetcode.com/problems/n-th-tribonacci-number/submissions/
-// TODO: implement MEMOIZATION to recursive algos
 
 class Solution {    
 public:
 	int tribonacci(int n) {
-		return trib_iterative(n); // just select whatever you need
+		return trib_recursive_memo(n); // just select whatever you need
 	};
 	
 	int trib_iterative(int n) {
 		int TR_0 = 0;
 		int TR_1 = 1;
-        int TR_2 = 1;
+		int TR_2 = 1;
 		int next, current, previous, evenearlier;
 		switch (n) {
 			case 0:	return TR_0;
 			case 1:	return TR_1;
-            case 2:	return TR_2;
+			case 2:	return TR_2;
 			default:
 				current = TR_2;
 				previous = TR_1;
-                evenearlier = TR_0;
+				evenearlier = TR_0;
 				for (int i = 3; i <= n; i++) {
 					next = evenearlier + previous + current;
 					evenearlier = previous;
-                    previous = current;
+					previous = current;
 					current = next;                    
 				}
 				return next;
@@ -34,7 +33,7 @@ public:
 		switch (n) {
 			case 0: return 0;
 			case 1:
-            case 2: return 1;
+			case 2: return 1;
 			default:
 				return trib_recursive_switch(n-3) + trib_recursive_switch(n-2) + trib_recursive_switch(n-1);
 		};
@@ -45,4 +44,18 @@ public:
 		if (n <= 2) return 1;
 		return trib_recursive_if(n-3) + trib_recursive_if(n-2) + trib_recursive_if(n-1);
 	};
+
+	vector<int> memo;
+	Solution(): memo(37, 0) {};
+	
+	int trib_recursive_memo(int n) {
+		if (n <= 0) return 0;
+		if (n <= 2) return 1;
+		
+		if (memo[n-1] == 0) memo[n-1] = trib_recursive_memo(n-1);
+		if (memo[n-2] == 0) memo[n-2] = trib_recursive_memo(n-2);
+		if (memo[n-3] == 0) memo[n-3] = trib_recursive_memo(n-3);
+		
+		return memo[n-3] + memo[n-2] + memo[n-1];
+  	};
 };
