396f8caaf499c0d6e49bcbaebf013939a4ca4856
Brawlence <42910943+Brawlence@users.noreply.github.com>
2022-06-28 17:01:11 +0300

    Added FILLER
    A short program to get 4096 hex zeroes and hex FFs for MCU RAM cleaning

---
+++ filler.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+int main () {
+  ofstream binary;
+  char buffer[4096] = {0};
+  binary.open("zeroes.bin", ios::out | ios::binary);
+  binary.write(buffer, 4096);
+  binary.close();
+
+  for (char & ele : buffer) ele = static_cast<char>(255);
+  binary.open("ffoes.bin", ios::out | ios::binary);
+  binary.write(buffer, 4096);
+  binary.close();
+
+  return 0;
+}
